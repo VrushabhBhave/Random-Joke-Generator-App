e6ed0b4f7c7d79c23a5d10da2a2614035c4da1f4
Vrushabhbhave <vrushabhbhave31@gmail.com>
2025-03-07 13:10:16 +0530

    push random joke generator code

---
+++ script.js
@@ -0,0 +1,35 @@
+const btn = document.querySelector("button");
+const jokeContainer = document.querySelector("#joke-container");
+const loadingPara = document.querySelector("p");
+const jokePara = document.createElement("p");
+const APIKEY = "EACPPhqMH4hQwbyYWnkfuw==6eGCb9ERKgRYHoxZ";
+
+btn.addEventListener("click", getJoke);
+
+async function getJoke() {
+    loadingPara.classList.add("visible");
+    jokePara.innerText = "";
+    try {
+        const header = {
+            headers: { "X-Api-Key": "EACPPhqMH4hQwbyYWnkfuw==6eGCb9ERKgRYHoxZ" },
+        };
+        const url = "https://api.api-ninjas.com/v1/dadjokes";
+        const dadjoke = await fetch(url, header);
+        const result = await dadjoke.json();
+        displayJoke(result);
+    }catch(error){
+        console.log(error);
+        loadingPara.innerText = "An error happened, try again later";
+        loadingPara.style.color = "red";
+    }
+}
+
+function displayJoke(result) {
+    loadingPara.classList.remove("visible");
+    loadingPara.classList.add("hidden");
+    jokeContainer.innerHTML = "";
+    console.log(result[0].joke);
+
+    jokePara.innerText = result[0].joke;
+    jokeContainer.append(jokePara);
+}
